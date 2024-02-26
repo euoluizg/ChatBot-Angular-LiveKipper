a580101d5c1d63d1369c4496408f1552a682c045
Luiz <117852519+euoluizg@users.noreply.github.com>
2024-02-25 22:24:14 -0300

    feat: create initial chat structure

--- ChatBot/src/app/components/chat-suggestions/chat-suggestions.component.ts
+++ ChatBot/src/app/components/chat-suggestions/chat-suggestions.component.ts
@@ -1,14 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { QuestionComponent } from '../../icons/question/question.component';
+import { HistoryComponent } from '../../icons/history/history.component';
+import { CuriosityComponent } from '../../icons/curiosity/curiosity.component';
 
 @Component({
-  selector: 'app-chat-suggestions',
+  selector: 'chat-suggestions',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    QuestionComponent,
+    HistoryComponent,
+    CuriosityComponent
+  ],
   templateUrl: './chat-suggestions.component.html',
   styleUrl: './chat-suggestions.component.scss'
 })
 export class ChatSuggestionsComponent {
-  initialQuestions: string[] = [
-    
+  suggestionTopics = [
+    {
+      title: "Dúvidas",
+      icon: 'doubt',
+      questions: [
+        "Qual o valor para entrar no museu?",
+        "Quando o museu está aberto?",
+      ]
+    },
+    {
+      title: "Curiosidades",
+      icon: 'curiosity',
+      questions: [
+        "Quem era o prefeito na época da criação?",
+        "Quantos cômodos existem no museu?",
+        "Quantas peças estão exibidas no museu?",
+      ]
+    },
+    {
+      title: "Historia",
+      icon: 'history',
+      questions: [
+        "Quando o museu foi criado?",
+        "Qual o estilo arquitetônico do prédio?",
+        "Quem foi o arquiteto do prédio?"
+      ]
+    }
   ]
 }
--- ChatBot/src/app/page/chat/chat.component.ts
+++ ChatBot/src/app/page/chat/chat.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { ArrowLeftComponent } from '../../icons/arrow-left/arrow-left.component';
 import { MuseumComponent } from '../../icons/museum/museum.component';
+import { ChatSuggestionsComponent } from '../../components/chat-suggestions/chat-suggestions.component';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   imports: [
     ArrowLeftComponent,
-    MuseumComponent
+    MuseumComponent,
+    ChatSuggestionsComponent
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
