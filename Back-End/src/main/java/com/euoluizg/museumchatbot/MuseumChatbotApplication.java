9f20f360a767740d68dd1595bc35ae12c5328e32
Luiz <117852519+euoluizg@users.noreply.github.com>
2024-02-29 13:15:46 -0300

    feat: adjust controller to be post method

--- Back-End/src/main/java/com/euoluizg/museumchatbot/MuseumChatbotApplication.java
+++ Back-End/src/main/java/com/euoluizg/museumchatbot/MuseumChatbotApplication.java
@@ -1,4 +1,4 @@
-package com.kipper.museumchatbot;
+package com.euoluizg.museumchatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ Back-End/src/main/java/com/euoluizg/museumchatbot/controllers/FaqController.java
@@ -0,0 +1,25 @@
+package com.euoluizg.museumchatbot.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.euoluizg.museumchatbot.dto.MessageRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/chat")
+public class FaqController {
+    
+    @PostMapping
+    public ResponseEntity<String> answerQuestion(@RequestBody MessageRequest request) {
+        System.out.println(request.message());
+        return ResponseEntity.ok("oi deu certo");
+    }
+    
+}
