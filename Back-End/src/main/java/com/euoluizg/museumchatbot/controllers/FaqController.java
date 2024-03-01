5f31917d7514d3ab47816d2b74b0b74bd1be1676
Luiz <117852519+euoluizg@users.noreply.github.com>
2024-03-01 12:50:33 -0300

    db

--- Back-End/src/main/java/com/euoluizg/museumchatbot/controllers/FaqController.java
+++ Back-End/src/main/java/com/euoluizg/museumchatbot/controllers/FaqController.java
@@ -4,13 +4,9 @@ package com.euoluizg.museumchatbot.controllers;
 import com.euoluizg.museumchatbot.dto.MessageResponse;
 import com.euoluizg.museumchatbot.services.FaqService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.euoluizg.museumchatbot.dto.MessageRequest;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -22,6 +18,7 @@ public class FaqController {
         this.faqService = faqService;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping
     public ResponseEntity<MessageResponse> answerQuestion(@RequestBody MessageRequest request) {
         String answer = this.faqService.getAnswer(request.message());
