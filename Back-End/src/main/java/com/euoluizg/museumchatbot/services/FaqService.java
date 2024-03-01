736ae354ec047d7cf2b74a2dbafbeca73397b5f8
Luiz <117852519+euoluizg@users.noreply.github.com>
2024-03-01 11:36:02 -0300

    db

---
+++ Back-End/src/main/java/com/euoluizg/museumchatbot/services/FaqService.java
@@ -0,0 +1,28 @@
+package com.euoluizg.museumchatbot.services;
+
+import com.euoluizg.museumchatbot.domain.FaqAnswer;
+import com.euoluizg.museumchatbot.utils.FaqAnswers;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class FaqService {
+    final private FaqAnswers faqAnswers = new FaqAnswers();
+
+    public String getAnswer(String question){
+        String[] words = question.toLowerCase().split("\\s+");
+        List wordsList = Arrays.asList(words);
+
+        for (FaqAnswer entry : faqAnswers.getAnswers()) {
+            for (String keyword : entry.getKeywords()) {
+                if (wordsList.contains(keyword.toLowerCase())) {
+                    return entry.getAnswer();
+                }
+            }
+        }
+
+        return faqAnswers.getDefaultAnswer();
+    }
+}
