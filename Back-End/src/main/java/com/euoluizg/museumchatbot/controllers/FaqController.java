63cd70fa6a1ec055874ea26cd359c1bfadcadf14
Luiz <117852519+euoluizg@users.noreply.github.com>
2024-03-01 11:33:19 -0300

    refector: change FaqService to final

--- Back-End/src/main/java/com/euoluizg/museumchatbot/controllers/FaqController.java
+++ Back-End/src/main/java/com/euoluizg/museumchatbot/controllers/FaqController.java
@@ -3,7 +3,6 @@ package com.euoluizg.museumchatbot.controllers;
 
 import com.euoluizg.museumchatbot.dto.MessageResponse;
 import com.euoluizg.museumchatbot.services.FaqService;
-import com.euoluizg.museumchatbot.utils.FaqAnswers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +11,12 @@ import com.euoluizg.museumchatbot.dto.MessageRequest;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/chat")
 public class FaqController {
-    private FaqService faqService;
+    final private FaqService faqService;
 
     public FaqController(FaqService faqService){
         this.faqService = faqService;
